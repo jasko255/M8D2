fd2fb5ea90748af67eeebe27b15882d110ae315b
jasko255 <jasko255@gmail.com>
2021-09-28 16:13:05 +0200

    schema

--- src/blogs/schema.js
+++ src/blogs/schema.js
@@ -1,23 +1,6 @@
 import mongoose from "mongoose";
 
-/**
- * 
- * {
-	    
-	    "category": "ARTICLE CATEGORY",
-	    "title": "ARTICLE TITLE",
-	    "cover":"ARTICLE COVER (IMAGE LINK)",
-	    "readTime": {
-	      "value": 2,
-	      "unit": "minute"
-	    },
-	    "author": {
-	      "name": "AUTHOR NAME",
-	      "avatar":"AUTHOR AVATAR LINK"
-	    },
-	    "content": "HTML",         
-}
- */
+
 
 const schema = new mongoose.Schema(
   {
